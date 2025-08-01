b8e512636b15fde338f7ef703eaa658cd596b40c
seosebin <seominbin@gmail.com>
2025-08-01 16:30:55 +0900

    feat: Router에 홈쇼핑 메인 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Main from './pages/HomeShoppingMain';
+import Login from './pages/user/Login';
+import Signup from './pages/user/Signup';
+import Main from './pages/home_shopping/HomeShoppingMain';
 
 function App() {
   return (
