4e0143afff6ee4c0320493db1394237e42b13899
seosebin <seominbin@gmail.com>
2025-08-01 16:34:33 +0900

    feat: Bottom Nav 추가

--- src/layout/BottomNav.js
+++ src/layout/BottomNav.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import '../styles/bottom_nav.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/bottom_nav.css";
+import bottomNavImage from "../assets/bottom_navigation.gif";
 
 const BottomNav = () => {
   const location = useLocation();
 
   return (
     <nav className="bottom-nav">
-      <Link to="/main" className={location.pathname === '/main' ? 'active' : ''}>
+      <Link
+        to="/kok"
+        className={`nav-item ${location.pathname === "/kok" ? "active" : ""}`}
+      >
         콕 쇼핑몰
       </Link>
 
-      <Link to="/recipe" className={location.pathname === '/recipe' ? 'active' : ''}>
-        레시피추천
+      <Link
+        to="/recipe"
+        className={`nav-item ${location.pathname === "/recipe" ? "active" : ""}`}
+      >
+        레시피 추천
       </Link>
 
-      <Link to="/special" className="round-button">
-        +
+      <Link
+        to="/main"
+        className="image-button-wrapper"
+        style={{ marginTop: "-80px", position: "relative" }} // position: relative 꼭 필요
+      >
+        <img src={bottomNavImage} alt="메인 버튼" className="image-button" />
+        <span
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "24px",
+            pointerEvents: "none", 
+            userSelect: "none",
+          }}
+        >
+          혹
+        </span>
       </Link>
 
-      <Link to="/favorites" className={location.pathname === '/favorites' ? 'active' : ''}>
+      <Link
+        to="/likes"
+        className={`nav-item ${location.pathname === "/likes" ? "active" : ""}`}
+      >
         찜
       </Link>
 
-      <Link to="/mypage" className={location.pathname === '/mypage' ? 'active' : ''}>
+      <Link
+        to="/mypage"
+        className={`nav-item ${location.pathname === "/mypage" ? "active" : ""}`}
+      >
         마이페이지
       </Link>
     </nav>
