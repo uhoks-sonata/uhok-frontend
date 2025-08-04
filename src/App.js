d173f5cfc5c877b7f2746652c343c7eb15d2126d
joseonyeong <whtjsdud54@naver.com>
2025-08-04 16:13:50 +0900

    Feat: 장바구니 및 알림 시 개수 표시

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './styles/App.css';
 import Login from './pages/user/Login';
 import Signup from './pages/user/Signup';
 import Main from './pages/home_shopping/HomeShoppingMain';
+import RecommendRecipe from './features/kok/bell_bucket_test';
+import { NotificationProvider } from './layout/HeaderNav';
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/main" element={<Main />} />
-          </Routes>
-        </Router>
+    <NotificationProvider>
+      <div className="wrapper">
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/main" element={<Main />} />
+              <Route path="/test" element={<RecommendRecipe />} />
+            </Routes>
+          </Router>
+        </div>
       </div>
-    </div>
+    </NotificationProvider>
   );
 }
 
