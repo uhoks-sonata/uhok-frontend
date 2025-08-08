8f241a788b24a2b9d22fd884726251738646fbcb
joseonyeong <whtjsdud54@naver.com>
2025-08-08 12:39:42 +0900

    Fix: proxy port 관리

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,21 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  const target = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9000';
+  const target =
+    process.env.API_PROXY_TARGET ||
+    process.env.REACT_APP_API_BASE_URL ||
+    'http://localhost:9000';
 
-  app.use(
-    ['/api', '/auth'].map((context) =>
-      createProxyMiddleware(context, {
+  const contexts = ['/api', '/auth'];
+  contexts.forEach((context) => {
+    app.use(
+      context,
+      createProxyMiddleware({
         target,
         changeOrigin: true,
         secure: false,
         logLevel: 'silent',
       })
-    )
-  );
+    );
+  });
 };
